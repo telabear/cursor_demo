8bcd781c2bf6fba68e2a71c068c1c9ba1ee65f18
telebear <czm8219@163.com>
2025-06-09 15:28:52 +0800

    add method

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // 1 构建原始的数组
+        String[] strArray = new String[] { "i", "love" };
+        // 2 通过Arrays.asList 工具把数组转成List 集合
+        List<String> strList = Arrays.asList(strArray);
+        // 3 往集合中添加元素
+        strList.add("you");
     }
 }
